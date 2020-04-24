a1e5ad631070e501e1d6898a752f11271e0168b0
Imran Hadid <imranhadid03@gmail.com>
2020-04-24 10:48:13 +0600

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->char('name');
+            $table->char('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->char('password');
+            $table->mediumText('auth_token');
             $table->rememberToken();
             $table->timestamps();
         });
